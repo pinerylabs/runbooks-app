b9d97fe851c4af964d2e0264eaa7ade95a097ba0
Andrew Miner <andrewminer@mac.com>
2021-01-18 11:09:26 -0700

    Deployment of 0ce7a00 at 2021-01-18T18:09:26.534326Z

--- docs/scripts/context.js
+++ docs/scripts/context.js
@@ -2,10 +2,10 @@
 
 window.pinerylabs = {
     application: "runbooks.app",
-    deployed_on: "2021-01-13T15:42:06.653063Z",
+    deployed_on: "2021-01-18T18:09:26.534326Z",
     environment: "production",
-    git_hash:    "2a04db9",
+    git_hash:    "0ce7a00",
     google_id:   "",
     silent:      false,
-    version:     "1.0.0"
+    version:     "1.1.0"
 };
